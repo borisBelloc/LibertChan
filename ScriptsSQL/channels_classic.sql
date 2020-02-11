43b3e2d147b27a6a0668350c96d22e0d26deb648
Boris BELLOC <bb.itwork@gmail.com>
2020-02-11 17:23:03 +0100

    update

--- ScriptsSQL/channels_classic.sql
+++ ScriptsSQL/channels_classic.sql
@@ -9,7 +9,10 @@ VALUES (1, 1000,  200, 'Actualités', 'ac', true, true, 'actualité, infos', 'IN
        (8, 1000,  200, 'Développement', 'de', true, true, 'Code, programmation, languages, frameworks...', 'INTEREST', ''),
        (9, 1000,  200, 'Graphisme', 'gr', true, true, 'Gimp, photoshop, paint...', 'INTEREST', ''),
        (10, 1000,  200, 'Informatique', 'in', true, true, '42', 'INTEREST', ''),
-       (11, 1000,  200, 'Aléatoire', 'al', false, true, 'Les WC du web', 'NSFW', ''),
+
+       (20, 1000,  200, 'Aléatoire', 'al', false, true, 'Les WC du web', 'NSFW', ''),
+
+       (30, 1000,  200, 'Suggestions concernant le site', 'si', true, true, 'Vos idées pour améliorer le site, demande de fonctionnalités', 'COMMUNITY', ''),
         
        (50, 1000,  200, 'France', 'fr', true, true, 'from France', 'REGIONAL', '../../../assets/images/flags/Europe/1_fr.png'),
        (51, 1000,  200, 'Québec', 'qc', true, true, 'la France lowcost', 'REGIONAL', '../../../assets/images/flags/Amerique/2_qc.png'),
@@ -19,4 +22,13 @@ VALUES (1, 1000,  200, 'Actualités', 'ac', true, true, 'actualité, infos', 'IN
 
        (55, 1000,  200, 'Haïti', 'ht', true, true, 'Fruits exotiques', 'REGIONAL', '../../../assets/images/flags/Amerique/11_ht.png'),
 
-       (56, 1000,  200, 'Liban', 'lb', true, true, 'no more banks', 'REGIONAL', '../../../assets/images/flags/asie/27_lu.png');
+       (56, 1000,  200, 'Liban', 'lb', true, true, 'no more banks', 'REGIONAL', '../../../assets/images/flags/Asie/19_lb.png'),
+
+       (57, 1000,  200, 'Algerie', 'dz', true, true, 'bouteflika', 'REGIONAL', '../../../assets/images/flags/Afrique/3_dz.png'),
+       (58, 1000,  200, 'Republique Democratique du Congo', 'Cng', true, true, 'kinshasa', 'REGIONAL', '../../../assets/images/flags/Afrique/2_cd.png'),
+       (59, 1000,  200, 'Maroc', 'mc', true, true, 'Mohammed IV', 'REGIONAL', '../../../assets/images/flags/Afrique/4_ma.png'),
+       (60, 1000,  200, 'Tunisie', 'tn', true, true, 'harissa', 'REGIONAL', '../../../assets/images/flags/Afrique/5_tn.png'),
+       (61, 1000,  200, 'Cameroun', 'cm', true, true, 'Dieudonné', 'REGIONAL', '../../../assets/images/flags/Afrique/7_cm.png'),
+       (62, 1000,  200, 'Cote divoire', 'ci', true, true, 'Cacao', 'REGIONAL', '../../../assets/images/flags/Afrique/9_ci.png');
+
+
