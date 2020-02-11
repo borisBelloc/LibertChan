5545f5a8eef15eecf3b76acaacc26d6495998f19
Boris BELLOC <bb.itwork@gmail.com>
2020-02-11 11:56:54 +0100

    added some country

--- ScriptsSQL/channels_classic.sql
+++ ScriptsSQL/channels_classic.sql
@@ -10,5 +10,13 @@ VALUES (1, 1000,  200, 'Actualités', 'ac', true, true, 'actualité, infos', 'IN
        (9, 1000,  200, 'Graphisme', 'gr', true, true, 'Gimp, photoshop, paint...', 'INTEREST', ''),
        (10, 1000,  200, 'Informatique', 'in', true, true, '42', 'INTEREST', ''),
        (11, 1000,  200, 'Aléatoire', 'al', false, true, 'Les WC du web', 'NSFW', ''),
-       (50, 1000,  200, 'France', 'fr', true, true, 'from France', 'REGIONAL', '../../../assets/images/Flags/Europe/1_fr.png'),
-       (51, 1000,  200, 'Québec', 'qc', true, true, 'la France lowcost', 'REGIONAL', '../../../assets/images/Flags/Amerique/2_qc.png');
+        
+       (50, 1000,  200, 'France', 'fr', true, true, 'from France', 'REGIONAL', '../../../assets/images/flags/Europe/1_fr.png'),
+       (51, 1000,  200, 'Québec', 'qc', true, true, 'la France lowcost', 'REGIONAL', '../../../assets/images/flags/Amerique/2_qc.png'),
+       (52, 1000,  200, 'Belgique', 'be', true, true, 'Frites', 'REGIONAL', '../../../assets/images/flags/Europe/6_be.png'),
+       (53, 1000,  200, 'Suisse', 'ch', true, true, 'monaie monaie', 'REGIONAL', '../../../assets/images/flags/Europe/21_ch.png'),
+       (54, 1000,  200, 'Luxembourg', 'lu', true, true, 'banques', 'REGIONAL', '../../../assets/images/flags/Europe/27_lu.png'),
+
+       (55, 1000,  200, 'Haïti', 'ht', true, true, 'Fruits exotiques', 'REGIONAL', '../../../assets/images/flags/Amerique/11_ht.png'),
+
+       (56, 1000,  200, 'Liban', 'lb', true, true, 'no more banks', 'REGIONAL', '../../../assets/images/flags/asie/27_lu.png');
